b5d5ed0ad8caf7399c2f2ba1a00f34c3f7784b5e
Harikesavan <harikesavan1@users.noreply.github.com>
2026-04-14 14:41:45 +0530

    fixed server for vercel

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,5 @@ app.use(express.json());
 app.use('/api/auth', authRoutes);
 app.use('/api/complaints', complaintRoutes);
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
-});
+
+
